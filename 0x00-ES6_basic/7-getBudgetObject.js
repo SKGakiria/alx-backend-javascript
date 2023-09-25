d0f8c8358c4a9601f49452ebcb9de3312f023dce
SKGakiria <swgakiria@gmail.com>
2023-09-25 20:51:50 +0000

    Added Task 7

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,11 @@
+// Modified function’s budget object to simply use the keyname instead
+
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}

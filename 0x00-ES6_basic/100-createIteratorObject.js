c0e32b4e47c33687b458fea4a60dc6a789ce6168
SKGakiria <swgakiria@gmail.com>
2023-09-26 12:11:06 +0000

    Fixed eslint errors

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,10 @@
 export default function createIteratorObject(report) {
-   let employee = [];
-   for (let [deprts, empls] of Object.entries(report.allEmployees)){
-        for (let emp of empls){
-            employee.push(emp);
-        }
-   }
-   return employee;
+  const employee = [];
+  // eslint-disable-next-line no-unused-vars
+  for (const [deprts, empls] of Object.entries(report.allEmployees)) {
+    for (const emp of empls) {
+      employee.push(emp);
+    }
+  }
+  return employee;
 }

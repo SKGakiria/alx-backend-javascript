1a29f8303763726ece145d38ebd9d40a26483f2e
SKGakiria <swgakiria@gmail.com>
2023-09-28 13:57:02 +0000

    Added Task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(nAmount) {
+    this._amount = nAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(nCurrency) {
+    this._currency = nCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}

d6ea98fb57664dea60f8b64bb78f6f61ae83c563
SKGakiria <swgakiria@gmail.com>
2023-11-22 15:38:23 +0000

    Added Task 0 - Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,4 @@
+// Function to round two items and return the sum of it
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,32 @@
+// Test cases for function containing the sum of a and b
+const assert = require("assert");
+const { it, describe } = require("mocha");
+const calculateNumber = require("./0-calcul");
+
+describe("calculateNumber()", function() {
+
+    it(`checking if numbers rounded`, function() {
+      const res = calculateNumber(1.0, 2.0);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers rounded`, function() {
+      const res = calculateNumber(1.0, 2.4);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers rounded`, function() {
+      const res = calculateNumber(2.6, 2.0);
+      assert.strictEqual(res, 5);
+    });
+    it(`checking if numbers rounded`, function() {
+      const res = calculateNumber(0, 0);
+      assert.strictEqual(res, 0);
+    });
+    it(`checking if numbers rounded`, function() {
+      const res = calculateNumber(-1.5, -1.8);
+      assert.strictEqual(res, -3);
+    });
+    it(`checking if numbers rounded`, function() {
+      const res = calculateNumber(2.6, 2.5);
+	assert.strictEqual(res, 6);
+    });
+});

21e6b1380d426229f72252bed31dde26e84adf31
SKGakiria <swgakiria@gmail.com>
2023-11-23 09:41:43 +0000

    Added Task 3 - Spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+// Validates the usage of the Utils function
+const sinon = require('sinon');
+const Utils = require('./utils');
+const expect = require("chai").expect;
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe("sendPaymentRequestToApi", function() {
+    it("checks Utils.calculateNumber was called once", function() {
+	const spy = sinon.spy(Utils, "calculateNumber");
+
+	sendPaymentRequestToApi(80, 20);
+
+	expect(spy.calledOnce).to.be.true;
+	spy.restore();
+    });
+});

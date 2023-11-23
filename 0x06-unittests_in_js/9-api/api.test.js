0b4dcb7f8a152da7d82edd9ecb9606834f25c3fb
SKGakiria <swgakiria@gmail.com>
2023-11-23 13:12:00 +0000

    Added Task 9 - Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,41 @@
+// Testing for Regex integration
+const request = require('request');
+const expect = require("chai").expect;
+
+describe("Index page", function() {
+  const API_URL = 'http://localhost:7865';
+
+  it('GET / returns correct response', function(done) {
+    request.get(`${API_URL}/`, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });  
+});
+
+describe("Cart page", function() {
+  const API_URL = 'http://localhost:7865';
+
+  it('GET /cart/:id returns correct response for valid :id', function(done) {
+    request.get(`${API_URL}/cart/54`, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 54');
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns 404 response for negative number values in :id', function(done) {
+    request.get(`${API_URL}/cart/-54`, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns 404 response for NOT a number value in :id', function(done) {
+    request.get(`${API_URL}/cart/lock`, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+});

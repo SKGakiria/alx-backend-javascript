5921d2de126dd60ceea8c2500f7752ce492056c3
SKGakiria <swgakiria@gmail.com>
2023-11-24 08:31:54 +0000

    Fixed lint issues on Task 0 test file

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,31 +1,32 @@
 // Test cases for function containing the sum of a and b
-const assert = require("assert");
-const calculateNumber = require("./0-calcul");
+/* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable jest/expect-expect */
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
 
-describe("calculateNumber", function() {
-
-    it(`checking if numbers rounded`, function() {
-      const res = calculateNumber(1.0, 2.0);
-      assert.strictEqual(res, 3);
-    });
-    it(`checking if numbers rounded`, function() {
-      const res = calculateNumber(1.0, 2.4);
-      assert.strictEqual(res, 3);
-    });
-    it(`checking if numbers rounded`, function() {
-      const res = calculateNumber(2.6, 2.0);
-      assert.strictEqual(res, 5);
-    });
-    it(`checking if numbers rounded`, function() {
-      const res = calculateNumber(0, 0);
-      assert.strictEqual(res, 0);
-    });
-    it(`checking if numbers rounded`, function() {
-      const res = calculateNumber(-1.5, -1.8);
-      assert.strictEqual(res, -3);
-    });
-    it(`checking if numbers rounded`, function() {
-      const res = calculateNumber(2.6, 2.5);
-	assert.strictEqual(res, 6);
-    });
+describe('calculateNumber', () => {
+  it('checking if numbers rounded', () => {
+    const res = calculateNumber(1.0, 2.0);
+    assert.strictEqual(res, 3);
+  });
+  it('checking if numbers rounded', () => {
+    const res = calculateNumber(1.0, 2.4);
+    assert.strictEqual(res, 3);
+  });
+  it('checking if numbers rounded', () => {
+    const res = calculateNumber(2.6, 2.0);
+    assert.strictEqual(res, 5);
+  });
+  it('checking if numbers rounded', () => {
+    const res = calculateNumber(0, 0);
+    assert.strictEqual(res, 0);
+  });
+  it('checking if numbers rounded', () => {
+    const res = calculateNumber(-1.5, -1.8);
+    assert.strictEqual(res, -3);
+  });
+  it('checking if numbers rounded', () => {
+    const res = calculateNumber(2.6, 2.5);
+    assert.strictEqual(res, 6);
+  });
 });
